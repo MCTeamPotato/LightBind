74971afa469f6f7fa760289a469dc407bc9402a2
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-09-25 10:31:35 +0800

    Initial commit

---
+++ src/main/java/me/kall/lightbind/LightBind.java
@@ -0,0 +1,28 @@
+package me.kall.lightbind;
+
+import com.google.common.collect.Lists;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import me.kall.jsonate.api.JsonConfig;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(LightBind.MOD_ID)
+public final class LightBind {
+    public static final String MOD_ID = "lightbind";
+
+    public static final JsonConfig CONFIG = JsonConfig.create(MOD_ID, "1.0.0")
+            .put("BindPair", Lists.newArrayList("cataclysm:flame_strike;240"))
+            .initialize();
+
+    public static final Object2IntMap<ResourceLocation> BIND_PAIRS = new Object2IntOpenHashMap<>();
+
+    static {
+        CONFIG.getStream("BindPair", String.class).forEach(entry -> {
+            String[] parts = entry.split(";");
+            String name = parts[0];
+            int light = Integer.parseInt(parts[1]);
+            BIND_PAIRS.put(ResourceLocation.tryParse(name), light);
+        });
+    }
+}
---
+++ src/main/java/me/kall/lightbind/mixin/EntityLighterMixin.java
@@ -0,0 +1,21 @@
+package me.kall.lightbind.mixin;
+
+import me.jellysquid.mods.sodium.client.model.light.EntityLighter;
+import me.jellysquid.mods.sodium.client.render.entity.EntityLightSampler;
+import me.kall.lightbind.LightBind;
+import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EntityLighter.class)
+public class EntityLighterMixin {
+    @Inject(method = "getBlendedLight", at = @At("HEAD"), cancellable = true)
+    private static <T extends Entity> void bind$getLight(EntityLightSampler<T> lighter, @NotNull T entity, float tickDelta, CallbackInfoReturnable<Integer> cir) {
+        int bind = LightBind.BIND_PAIRS.getOrDefault(entity.getType().getRegistryName(), -1);
+        if (bind == -1) return;
+        cir.setReturnValue(bind);
+    }
+}
