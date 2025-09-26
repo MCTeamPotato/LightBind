505e8b959d50165b635942359ea2fb71274e4b08
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2025-09-26 16:21:19 +0800

    new cursed config option

--- src/main/java/me/kall/lightbind/LightBind.java
+++ src/main/java/me/kall/lightbind/LightBind.java
@@ -11,18 +11,22 @@ import net.minecraftforge.fml.common.Mod;
 public final class LightBind {
     public static final String MOD_ID = "lightbind";
 
-    public static final JsonConfig CONFIG = JsonConfig.create(MOD_ID, "1.0.0")
+    public static final JsonConfig CONFIG = JsonConfig.create(MOD_ID, "1.0.1")
             .put("BindPair", Lists.newArrayList("cataclysm:flame_strike;240"))
+            .put("AllEntitiesLightest", false)
             .initialize();
 
     public static final Object2IntMap<ResourceLocation> BIND_PAIRS = new Object2IntOpenHashMap<>();
+    public static final boolean LIGHTEST = CONFIG.getBoolean("AllEntitiesLightest");
 
     static {
-        CONFIG.getStream("BindPair", String.class).forEach(entry -> {
-            String[] parts = entry.split(";");
-            String name = parts[0];
-            int light = Integer.parseInt(parts[1]);
-            BIND_PAIRS.put(ResourceLocation.tryParse(name), light);
-        });
+        if (!CONFIG.getBoolean("AllEntitiesLightest")) {
+            CONFIG.getStream("BindPair", String.class).forEach(entry -> {
+                String[] parts = entry.split(";");
+                String name = parts[0];
+                int light = Integer.parseInt(parts[1]);
+                BIND_PAIRS.put(ResourceLocation.tryParse(name), light);
+            });
+        }
     }
 }
--- src/main/java/me/kall/lightbind/mixin/EntityLighterMixin.java
+++ src/main/java/me/kall/lightbind/mixin/EntityLighterMixin.java
@@ -14,6 +14,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class EntityLighterMixin {
     @Inject(method = "getBlendedLight", at = @At("HEAD"), cancellable = true)
     private static <T extends Entity> void bind$getLight(EntityLightSampler<T> lighter, @NotNull T entity, float tickDelta, CallbackInfoReturnable<Integer> cir) {
+        if (LightBind.LIGHTEST) {
+            cir.setReturnValue(240);
+            return;
+        }
+        if (LightBind.BIND_PAIRS.isEmpty()) return;
         int bind = LightBind.BIND_PAIRS.getOrDefault(entity.getType().getRegistryName(), -1);
         if (bind == -1) return;
         cir.setReturnValue(bind);
